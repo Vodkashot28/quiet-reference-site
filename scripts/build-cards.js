d5251eb44d2d7373c56d4676963f525c9753cb5e
Vodkashot28 <jnalair28@gmail.com>
2026-04-21 15:26:10 +0000

    achoring new cid

--- scripts/build-cards.js
+++ scripts/build-cards.js
@@ -2,12 +2,12 @@ const { Resvg } = require('@resvg/resvg-js');
 const fs = require('fs');
 const path = require('path');
 
-const dir = 'assets/social';
+const dir = '_site/assets/social';
 fs.readdirSync(dir).forEach(file => {
   if (file.endsWith('.svg')) {
     const input = path.join(dir, file);
     const output = path.join(dir, file.replace('.svg', '.png'));
-    const svg = fs.readFileSync(input);
+    const svg = fs.readFileSync(input, 'utf8'); // important: read as string
     const resvg = new Resvg(svg);
     const pngData = resvg.render().asPng();
     fs.writeFileSync(output, pngData);
